4eadf1e34823318eb53278119fa104eb9b4672db
wadeling <wadeling@gmail.com>
2021-05-31 15:50:18 +0800

    add harbro api v1 test

--- test_api.sh
+++ test_api.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-username=wade
-password=xxx
+#username=admin
+#password=Harbor12345
 
-harborurl="http://xxx.xxx.xxx.xxx:80/"
+harborurl="http://192.168.208.195:80/"
+
+#api v2
 apiversion="api/v2.0/"
+
+#api v1
+#apiversion="api"
 
 # get projects
 #requrl="${harborurl}${apiversion}projects"
@@ -15,6 +20,11 @@ apiversion="api/v2.0/"
 #requrl="${harborurl}${apiversion}statistics"
 #curl -v --user ${username}:${password} $requrl 
 
+# depreted get repositories
+#requrl="${harborurl}${apiversion}repositories"
+#curl -v --user ${username}:${password} $requrl 
+
+
 # get project repos
 #projectname=test
 #requrl="${harborurl}${apiversion}projects/{$projectname}/repositories"
@@ -24,20 +34,26 @@ apiversion="api/v2.0/"
 #projectname=test
 #reponame=test
 #requrl="${harborurl}${apiversion}projects/{$projectname}/repositories/${reponame}"
-#curl -v --user ${username}:${password} $requrl 
+#curl -v -k --user ${username}:${password} $requrl 
 
 #get repo artifacts(tags,digest)
-#projectname=test
-#reponame=test
-#requrl="${harborurl}${apiversion}projects/{$projectname}/repositories/${reponame}/artifacts"
-#curl -v --user ${username}:${password} $requrl 
-
-#get image info 
 projectname=test
 reponame=test
-#ref can be tag or image digest
-ref=tom
-requrl="${harborurl}${apiversion}projects/{$projectname}/repositories/${reponame}/artifacts/${ref}"
-curl -v --user ${username}:${password} $requrl 
+requrl="${harborurl}${apiversion}projects/{$projectname}/repositories/${reponame}/artifacts"
+curl -v -k --user ${username}:${password} $requrl 
 
+#get image info 
+#projectname=test
+#reponame=test
+#ref can be tag or image digest
+#ref=tom
+#requrl="${harborurl}${apiversion}projects/{$projectname}/repositories/${reponame}/artifacts/${ref}"
+#curl -v --user ${username}:${password} $requrl 
+
+
+#api v1 test
+# get tags
+#reponame="library/nginx"
+#requrl="${harborurl}${apiversion}/repositories/${reponame}/tags?detail=true"
+#curl -v -k --user ${username}:${password} $requrl 
 
